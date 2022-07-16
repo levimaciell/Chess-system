23deb0062bfe173b435bf9608e0e11960088336f
levimaciell <levimaciell1221@gmail.com>
2022-07-16 15:48:35 -0300

    chess exception and chess position

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -4,7 +4,6 @@ import chess.enums.Color;
 import chess.pieces.King;
 import chess.pieces.Rook;
 import entities.boardgame.Board;
-import entities.boardgame.Position;
 
 public class ChessMatch {
 
@@ -35,9 +34,13 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0,4));
-		board.placePiece(new King(board, Color.WHITE), new Position(7,4));
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e', 1, new King(board, Color.WHITE));
 	}
 }
---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,46 @@
+package chess;
+
+import chess.exception.ChessException;
+import entities.boardgame.Position;
+
+public class ChessPosition {
+
+	private int row;
+	private char column;
+	
+	public ChessPosition(char column, int row) {
+		if(column < 'a' || column > 'h' || row < 1 || row > 8) {
+			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8");
+		}
+		
+		this.row = row;
+		this.column = column;
+	}
+	
+
+	public int getRow() {
+		return row;
+	}
+
+	public char getColumn() {
+		return column;
+	}
+
+	protected Position toPosition() {
+		int toPositionRow = 8 - row;
+		int toPositionColumn = column - 'a';
+		return new Position(toPositionRow, toPositionColumn);
+	}
+	
+	protected static ChessPosition fromPosition(Position position) {
+		int fromPositionRow = 8 - position.getRow();
+		char fromPositionColumn = (char) ((char)'a' - position.getColumn());
+		
+		return new ChessPosition(fromPositionColumn, fromPositionRow);
+	}
+	
+	@Override
+	public String toString() {
+		return "" + column + row;
+	}
+}
